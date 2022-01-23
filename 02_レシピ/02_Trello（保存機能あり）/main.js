90c7b0e5a5cb26335970469e1eb4165cd7c9355b
SHUNGO_OKA <shungoh120216@gmail.com>
2022-01-23 14:11:15 +0900

    [Trello (保存機能なし)]の発展問題 ★ - エンターキーを解きました。

--- 02_レシピ/02_Trello（保存機能あり）/main.js
+++ 02_レシピ/02_Trello（保存機能あり）/main.js
@@ -2,6 +2,31 @@ const inputElement = document.getElementById("input-todo")
 const container = document.getElementById("cards-container")
 const addButton = document.getElementById("add-button")
 
+inputElement.addEventListener("keypress", enter)
+function enter(e) {
+  if (e.keyCode === 13) {
+    const text = inputElement.value
+
+    const card = document.createElement("div")
+    card.className = "card"
+
+    const todo = document.createElement("div")
+    todo.className = "todo"
+    todo.textContent = text
+
+    card.append(todo)
+
+    const deleteButton = document.createElement("div")
+    deleteButton.className = "delete"
+    card.append(deleteButton)
+    deleteButton.onclick = function() {
+      card.remove()
+    }
+    container.append(card)
+    inputElement.value = ""
+  }
+}
+
 addButton.onclick = function() {
   const card = createdCard(inputElement.value)
   container.append(card)
@@ -24,6 +49,6 @@ const createdCard = function(text) {
   deleteButton.onclick = function() {
     card.remove(todo)
   }
-  // 12行目のcardに返っていくよ
+  // 31行目のcardに返っていくよ
   return card
 }
