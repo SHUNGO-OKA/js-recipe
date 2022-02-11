fcbbdf44e1e3d5a53795703171f832a0aafdce7f
SHUNGO_OKA <shungoh120216@gmail.com>
2022-02-12 01:16:38 +0900

    Vueで自販機を作っためっちゃ便利

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,59 +1,60 @@
 // 3の倍数
 const GENKIFunciton = (n) => {
-    for(let i = 1; i <= n; i++){
-        if (i % 3 === 0){
-            console.log(i + "!!!!!!!")
-        } else {
-            console.log(i)
-        }
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0) {
+      console.log(i + "!!!!!!!")
+    } else {
+      console.log(i)
     }
+  }
 }
 
 GENKIFunciton(100)
 
 // 発展★ - FizzBuzz
 const FizzBuzzFunction = (n) => {
-    for(let i = 1; i <= n; i++){
-        if (i % 15 === 0){
-            console.log(i + "FizzBuzz")
-        } else if (i % 5 === 0) {
-            console.log(i + "Buzz")
-        } else if (i % 3 === 0){
-            console.log(i + "Fizz")
-        } else {
-            console.log(i)
-        }
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log(i + "FizzBuzz")
+    } else if (i % 5 === 0) {
+      console.log(i + "Buzz")
+    } else if (i % 3 === 0) {
+      console.log(i + "Fizz")
+    } else {
+      console.log(i)
     }
+  }
 }
 
 FizzBuzzFunction(100)
 
 // 発展★★ - 3が大好き
 const IncludeThree = (n) => {
-    for(let i=1; i <= n; i++){
-        if (String(i).includes("3") || i % 3 === 0) {
-            console.log(i + "!!!!!!!!")
-        } else {
-            console.log(i)
-        }
+  for (let i = 1; i <= n; i++) {
+    if (String(i).includes("3") || i % 3 === 0) {
+      console.log(i + "!!!!!!!!")
+    } else {
+      console.log(i)
     }
+  }
 }
 
 IncludeThree(100)
 
 // 発展★★★ - 元気な秒針
 const GENKIClock = (n) => {
-    let number = 0;
-    setInterval(function(){
-        number++
-        if (number > n) {return};
-        if (number % 3 === 0) {
-            console.log(number + "!!!!!!")
-        } else {
-            console.log(number)
-        }
-    },1000)
+  let number = 0
+  setInterval(function() {
+    number++
+    if (number > n) {
+      return
+    }
+    if (number % 3 === 0) {
+      console.log(number + "!!!!!!")
+    } else {
+      console.log(number)
+    }
+  }, 1000)
 }
 
 GENKIClock(100)
- 
